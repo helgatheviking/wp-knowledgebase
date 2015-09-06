db8d7dd51750ee67bf7eae75ea426a00ab99f761
Kathy Darling <helgatheviking@gmail.com>
2015-09-06 15:17:28 -0400

    only update from old setting if there are no new settings

--- includes/kbe_upgrade.php
+++ includes/kbe_upgrade.php
@@ -20,8 +20,13 @@ function kbe_upgrade_plugin(){
     // major 1.1.0 upgrade
     if( version_compare( $db_version, '1.1.0', '<')) {
         
-     	// merge all options fields into 1 option
-     	$options = $permalinks = array();
+        // check if new $settings exist
+     	$settings = get_option( 'kbe_settings', array() );
+     	
+     	// merge all old options fields into 1 option
+     	if ( empty( $settings )) {
+     		
+	     	$permalinks = array();
 
     	global $wpdb;
 		$getSql = $wpdb->get_results("Select ID From $wpdb->posts Where post_content Like '%[kbe_knowledgebase]%' And post_type <> 'revision'");
@@ -29,37 +34,39 @@ function kbe_upgrade_plugin(){
 		foreach($getSql as $getRow) {
 		    $options['archive_page_id'] = $getRow->ID;
 		}
+			$settings['archive_page_id'] = $page_found && $page_found > 0 ? $page_found : 0;
+			$settings['article_qty'] = get_option( 'kbe_article_qty', 5 );
+			$settings['search_setting'] = get_option( 'kbe_search_setting', 0 );
+			$settings['breadcrumb_setting'] = get_option( 'kbe_breadcrumbs_setting', 0 );
+			$settings['sidebar_home'] = get_option( 'kbe_sidebar_home', 0 );
+			$settings['sidebar_inner'] = get_option( 'kbe_sidebar_inner', 0 );
+			$settings['comments_setting'] = get_option( 'kbe_comments_setting', 0 );
+			$settings['bgcolor'] = get_option( 'kbe_bgcolor', '' );
 
-		$options['article_qty'] = get_option( 'kbe_article_qty', 5 );
-		$options['search_setting'] = get_option( 'kbe_search_setting', 0 );
-		$options['breadcrumb_setting'] = get_option( 'kbe_breadcrumbs_setting', 0 );
-		$options['sidebar_home'] = get_option( 'kbe_sidebar_home', 0 );
-		$options['sidebar_inner'] = get_option( 'kbe_sidebar_inner', 0 );
-		$options['comments_setting'] = get_option( 'kbe_comments_setting', 0 );
-		$options['bgcolor'] = get_option( 'kbe_bgcolor', '' );
+			$slug = trim( get_option( 'kbe_plugin_slug' ) );
+			$permalinks['article_base'] = $slug ? $slug : 'knowledgebase';
 
-		$slug = trim( get_option( 'kbe_plugin_slug' ) );
-		$permalinks['article_base'] = $slug ? $slug : 'knowledgebase';
+			// delete old options
+			delete_option('kbe_bgcolor');
+		    delete_option('kbe_plugin_slug');
+		    delete_option('kbe_article_qty');
+		    delete_option('kbe_sidebar_home');
+		    delete_option('kbe_sidebar_inner');
+		    delete_option('kbe_search_setting');
+		    delete_option('kbe_comments_setting');
+		    delete_option('kbe_taxonomy_children');
+		    delete_option('kbe_breadcrumbs_setting');
+		    delete_option('widget_kbe_tags_widgets');
+		    delete_option('widget_kbe_search_widget');
+		    delete_option('widget_kbe_article_widget');
+		    delete_option('widget_kbe_category_widget');
+		    delete_option('kbe_permalinks');
 
-		// delete old options
-		delete_option('kbe_bgcolor');
-	    delete_option('kbe_plugin_slug');
-	    delete_option('kbe_article_qty');
-	    delete_option('kbe_sidebar_home');
-	    delete_option('kbe_sidebar_inner');
-	    delete_option('kbe_search_setting');
-	    delete_option('kbe_comments_setting');
-	    delete_option('kbe_taxonomy_children');
-	    delete_option('kbe_breadcrumbs_setting');
-	    delete_option('widget_kbe_tags_widgets');
-	    delete_option('widget_kbe_search_widget');
-	    delete_option('widget_kbe_article_widget');
-	    delete_option('widget_kbe_category_widget');
-	    delete_option('kbe_permalinks');
+			// update the new options
+			update_option( 'kbe_settings', $settings );
+			update_option( 'kbe_permalinks', $settings );
 
-		// update the new options
-		update_option( 'kbe_settings', $options );
-		update_option( 'kbe_permalinks', $options );
+		}
 
         // update the db version number
         update_option( $v, '1.1.0' );
